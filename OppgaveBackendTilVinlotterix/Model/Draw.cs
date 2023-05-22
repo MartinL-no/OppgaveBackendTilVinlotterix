cbb1c76c586cd9b5f63880d549b1d8e34c6c517d
MartinL-no <martin.lorimer@icloud.com>
2023-05-22 13:48:03 +0200

    remove fields from model

--- OppgaveBackendTilVinlotterix/Model/Draw.cs
+++ OppgaveBackendTilVinlotterix/Model/Draw.cs
@@ -4,19 +4,11 @@
     {
         public Guid Id { get; set; }
         public DateTime Time { get; set; }
-        public string Winners { get; set; }
-        public string Participants { get; set; }
 
         public Draw()
         {
             Id = Guid.NewGuid();
             Time = DateTime.Now;
-        }
-
-        public Draw(string winners, string participants) : this()
-        {
-            Winners = winners;
-            Participants = participants;
         }
     }
 }
